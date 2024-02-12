49fb476b12e4a5ea3aa7c82b759e56f78f4c96c5
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-02-12 21:17:57 +0500

    Conrtoller

---
+++ HackatonAPI/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using Hackaton.Service.DTOs;
+using Hackaton.Service.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackatonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService service;
+        public OrderController(IOrderService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync(OrderDto dto)
+            => Ok(await service.CreateAsync(dto));
+
+        
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+            => Ok(service.GetAll(p => p.Id != 0));
+
+
+        
+        [HttpPut("{id}")]
+        public async ValueTask<IActionResult> UpdateAsync([FromRoute] int id, OrderDto dto)
+            => Ok(service.Update(id, dto));
+
+        
+        [HttpGet("{id}")]
+        public async ValueTask<IActionResult> GetAsync([FromRoute] int id)
+            => Ok(await service.GetAsync(u => u.Id == id));
+
+        
+        [HttpDelete("{id}")]
+        public async ValueTask<IActionResult> DeleteAsync([FromRoute] int id)
+            => Ok(await service.DeleteAsync(p => p.Id == id));
+    }
+}
